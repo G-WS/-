145fe646e2fe3e1b91a9117adce005fdb97ddf9d
G-WS <48270961+G-WS@users.noreply.github.com>
2019-06-26 21:10:09 +0800

    Add files via upload
    jdbc连接桥

---
+++ Try.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Try {
+    public Connection getConnection(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+                    return DriverManager.getConnection("jdbc:mysql://local host:",user.passager/3306/wujiahao);
+
+        }catch Class
+    }
+}
